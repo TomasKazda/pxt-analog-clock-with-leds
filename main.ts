ccffd32de39ed6672460e77c099c9e697ec2077a
Tomáš Kazda <36702826+TomasKazda@users.noreply.github.com>
2021-03-12 23:39:04 +0100

    ring24 step

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
+let hoursLED = neopixel.create(DigitalPin.P1, 24, NeoPixelMode.RGB)
+hoursLED.showRainbow(0, 270)
+hoursLED.setPixelColor(9, neopixel.rgb(0, 0, 160))
+hoursLED.rotate(15)
+hoursLED.show() 
 
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+        hoursLED.rotate(1)
+        hoursLED.show()  
+})
+
+basic.forever(function () {
+	
+})
