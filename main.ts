6ea2f8a81527772864e77e3ebed4e5347bdf7e16
Tomáš Kazda <36702826+TomasKazda@users.noreply.github.com>
2021-03-13 19:05:30 +0100

    minutes

--- main.ts
+++ main.ts
@@ -1,14 +1,34 @@
-let hoursLED = neopixel.create(DigitalPin.P1, 24, NeoPixelMode.RGB)
-hoursLED.showRainbow(0, 270)
-hoursLED.setPixelColor(9, neopixel.rgb(0, 0, 160))
-hoursLED.rotate(15)
-hoursLED.show() 
-
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-        hoursLED.rotate(1)
-        hoursLED.show()  
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    basic.showNumber(servo1.getPulse())
 })
-
+input.onButtonPressed(Button.A, function () {
+    servo1.setAngle(2225)
+})
+input.onButtonPressed(Button.B, function () {
+    servo1.setPulseBy(-18)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    hoursLED.rotate(1)
+    hoursLED.show()
+})
+let hoursLED: neopixel.Strip = null
+let servo1: servoPWM.Servo = null
+hoursLED = neopixel.create(DigitalPin.P1, 24, NeoPixelMode.RGB)
+hoursLED.setPixelColor(0, neopixel.rgb(0, 0, 160))
+hoursLED.setPixelColor(12, neopixel.rgb(160, 0, 0))
+hoursLED.show()
+servo1 = servoPWM.createServo(AnalogPin.P0)
+servo1.setMinPulse(745)
+servo1.setMaxPulse(2225)
+servo1.setMaxAngle(2520)
+servo1.setAngle(2225)
+// if (input.buttonIsPressed(Button.A)) {
+// servo1.setAngleBy(-1)
+// basic.pause(20)
+// } else if (input.buttonIsPressed(Button.B)) {
+// servo1.setAngleBy(1)
+// basic.pause(20)
+// }
 basic.forever(function () {
 	
 })
