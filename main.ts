fe72af626767d1a60165cd4d6c94c6f0e6d163f7
Tomáš Kazda <36702826+TomasKazda@users.noreply.github.com>
2021-04-03 16:54:53 +0200

    stopwatch without stepper

--- main.ts
+++ main.ts
@@ -1,34 +1,56 @@
-input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
-    basic.showNumber(servo1.getPulse())
+input.onButtonPressed(Button.A, function() {
+    setPulse(60)
 })
-input.onButtonPressed(Button.A, function () {
-    servo1.setAngle(2225)
+
+input.onButtonPressed(Button.B, function() {
+    setPulse(1)
 })
-input.onButtonPressed(Button.B, function () {
-    servo1.setPulseBy(-18)
+input.onLogoEvent(TouchButtonEvent.LongPressed, function() {
+    reset()
 })
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    hoursLED.rotate(1)
-    hoursLED.show()
+pins.onPulsed(DigitalPin.P8, PulseValue.High, function () {
+    //serial.writeValue("time", control.millis())
+    setPulse(1)
 })
+
+function setPulse(pulses: number):void {
+    tick += pulses
+    // for (let i = 0; i < pulses; i++) {
+    //     if (++tick % 30 === 0)
+    //     {
+    //         hoursLED.rotate()
+    //     }
+    //     minsLED.rotate()
+    // }
+    // hoursLED.show()
+    // minsLED.show()
+    minsLED.showBarGraph(tick%60, 59)
+    hoursLED.showBarGraph(tick%720, 719)
+    if (tick === 720) tick = 0
+    led.plotBarGraph(tick, 720);
+}
+
+function reset():void {
+    tick = 0
+    minsLED.showBarGraph(tick%60, 59)
+    hoursLED.showBarGraph(tick%720, 719)
+    minsLED.setBrightness(32)
+    hoursLED.setBrightness(32)
+    //hoursLED.setPixelColor(0, neopixel.rgb(0, 0, 16))
+    //minsLED.setPixelColor(0, neopixel.rgb(0, 16, 0))
+    //minsLED.show()
+    //hoursLED.show()
+    led.plotBarGraph(tick, 720);
+}
+
+let tick: number = 0
 let hoursLED: neopixel.Strip = null
-let servo1: servoPWM.Servo = null
-hoursLED = neopixel.create(DigitalPin.P1, 24, NeoPixelMode.RGB)
-hoursLED.setPixelColor(0, neopixel.rgb(0, 0, 160))
-hoursLED.setPixelColor(12, neopixel.rgb(160, 0, 0))
-hoursLED.show()
-servo1 = servoPWM.createServo(AnalogPin.P0)
-servo1.setMinPulse(745)
-servo1.setMaxPulse(2225)
-servo1.setMaxAngle(2520)
-servo1.setAngle(2225)
-// if (input.buttonIsPressed(Button.A)) {
-// servo1.setAngleBy(-1)
-// basic.pause(20)
-// } else if (input.buttonIsPressed(Button.B)) {
-// servo1.setAngleBy(1)
-// basic.pause(20)
-// }
+let minsLED: neopixel.Strip = null
+hoursLED = neopixel.create(DigitalPin.P14, 24, NeoPixelMode.RGB)
+minsLED = neopixel.create(DigitalPin.P15, 60, NeoPixelMode.RGB)
+pins.setPull(DigitalPin.P8, PinPullMode.PullNone)
+reset()
+
 basic.forever(function () {
 	
 })
