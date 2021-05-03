2661f0826f823f3f517076c192619d7be2d77fc4
Tomáš Kazda <36702826+TomasKazda@users.noreply.github.com>
2021-05-03 23:08:10 +0200

    stepper smoothness

--- main.ts
+++ main.ts
@@ -1,32 +1,49 @@
 interval.OnInterval(999, function () {
-    if (running) {
-        setPulse(1)
+    if (running && !busy) {
+        let tmp = ++pulseStack
+        pulseStack = 0
+        setPulse(tmp)
+    } 
+    else if (running && busy) {
+        pulseStack++
+    } 
+    else if (pulseStack > 0)
+    {
+        let tmp = pulseStack
+        pulseStack = 0
+        setPulse(tmp)
     }
 })
 input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
     reset()
 })
 input.onButtonPressed(Button.A, function () {
-    setPulse(30)
+    if (!busy) {
+        pulseStack += 30
+        let tmp = pulseStack
+        pulseStack = 0
+        setPulse(tmp)
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    if (!reseting) running = !(running)
+    if (!busy) running = !(running)
 })
 pins.onPulsed(DigitalPin.P8, PulseValue.High, function () {
     // serial.writeValue("time", control.millis())
-    if (!(running) && !reseting) {
-        setPulse(1)
+    if (!(running) && !busy) {
+        pulseStack++
     }
 
     //hand calibration
     if (running) {
         let delta = tick % 60
-        if (delta == 0) magicbit.StepperDegree(magicbit.Steppers.STPM1, 3.2)
-        else if (delta == 1) magicbit.StepperDegree(magicbit.Steppers.STPM1, 6.55)
-        else if (delta == 58) magicbit.StepperDegree(magicbit.Steppers.STPM1, -3.2)
-        else if (delta > 1 && delta < 30) magicbit.StepperDegree(magicbit.Steppers.STPM1, 6.55 * delta)
-        else if (delta < 58) magicbit.StepperDegree(magicbit.Steppers.STPM1, -6.55 * delta)
         //serial.writeValue("div", delta)
+        busy = true
+        if (delta == 0) magicbit.StepperDegree(magicbit.Steppers.STPM1, 3.2)
+        else if (delta == 58) magicbit.StepperDegree(magicbit.Steppers.STPM1, -3.2)
+        else if (delta > 0 && delta < 30) magicbit.StepperDegree(magicbit.Steppers.STPM1, 5.8 * delta)
+        else if (delta < 58) magicbit.StepperDegree(magicbit.Steppers.STPM1, -5.8 * (58 - delta))
+        busy = false
     }
 })
 function setPulse (pulses: number) {
@@ -35,18 +52,32 @@ function setPulse (pulses: number) {
     let steps = pulses % 60;
     if (steps > 0) 
     {
-        tick -= steps
-        for (let i = 0; i < steps; i++)
-        {
-            tick++
-            if (tick % 4 == 0)
-                magicbit.StepperDegree(magicbit.Steppers.STPM1, 5.8)
-            else
-                magicbit.StepperDegree(magicbit.Steppers.STPM1, 6.55)
+        busy = true
+        let angle = 0;
+        if (steps < 6) {
+            tick -= steps
+            for (let i = 0; i < steps; i++)
+            {
+                tick++
+                if (tick % 4 == 0)
+                {
+                    angle += 5.7;
+                }
+                else
+                {
+                    angle += 6.45;
+                }
+            }
+        } else {
+            angle = steps * 5.8
         }
+        minsLED.showBarGraph(tick % 60, 59)
+        hoursLED.showBarGraph(tick % 720, 719)
+        //serial.writeValue("angle", angle)
+        magicbit.StepperDegree(magicbit.Steppers.STPM1, angle)
+        busy = false
     }
-    minsLED.showBarGraph(tick % 60, 59)
-    hoursLED.showBarGraph(tick % 720, 719)
+
     if (tick == 720) {
         tick = 0
     }
@@ -54,17 +85,13 @@ function setPulse (pulses: number) {
 }
 
 function reset () {
-    reseting = true
+    busy = true
     running = false
     tick = 0
     minsLED.showBarGraph(tick % 60, 59)
     hoursLED.showBarGraph(tick % 720, 719)
     minsLED.setBrightness(32)
     hoursLED.setBrightness(32)
-    // hoursLED.setPixelColor(0, neopixel.rgb(0, 0, 16))
-    // minsLED.setPixelColor(0, neopixel.rgb(0, 16, 0))
-    // minsLED.show()
-    // hoursLED.show()
     led.plotBarGraph(tick, 720)
     if (pins.digitalReadPin(DigitalPin.P8) != 0)
     {
@@ -75,17 +102,18 @@ function reset () {
         } while (pins.digitalReadPin(DigitalPin.P8) != 0)
         magicbit.StepperDegree(magicbit.Steppers.STPM1, 3)
     }
-    reseting = false
+    busy = false
 }
 let tick = 0
+let pulseStack = 0
 let running = false
-let reseting = false
+let busy = false
 let minsLED: neopixel.Strip = null
 let hoursLED: neopixel.Strip = null
 hoursLED = neopixel.create(DigitalPin.P14, 24, NeoPixelMode.RGB)
 minsLED = neopixel.create(DigitalPin.P15, 60, NeoPixelMode.RGB)
 pins.setPull(DigitalPin.P8, PinPullMode.PullNone)
-basic.pause(500)
+basic.pause(1000)
 reset()
 basic.forever(function () {
 	
